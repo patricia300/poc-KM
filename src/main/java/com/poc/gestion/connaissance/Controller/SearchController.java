a63a58d04a5c70a3bc9bf6752e7d507a2c39c76f
Patricia Rasoanambinina <patricia.rasoanambinina@cgi.com>
2025-03-19 13:56:53 +0100

    Ãfirst commit

---
+++ src/main/java/com/poc/gestion/connaissance/Controller/SearchController.java
@@ -0,0 +1,15 @@
+package com.poc.gestion.connaissance.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+    @GetMapping
+    public String search() {
+        return "search";
+    }
+}
